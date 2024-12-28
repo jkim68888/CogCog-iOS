a4eba64e8422336a919c438fd4b35a1bca40c2b6
jkim68888 <jkim68888@gmail.com>
2024-12-28 18:57:55 +0900

    Initial commit

---
+++ CogCog-iOS/CogCog_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CogCog_iOSApp.swift
+//  CogCog-iOS
+//
+//  Created by 김지현 on 12/28/24.
+//
+
+import SwiftUI
+
+@main
+struct CogCog_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CogCog-iOS
+ 
